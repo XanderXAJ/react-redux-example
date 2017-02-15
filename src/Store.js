e0cabfd3ff71e135cc8118ea6eee3429f658eb91
Alex Palmer <xanderx@xanderx.com>
2017-02-15 21:41:49 +0000

    Further justify react-immutable-state-invariant

--- src/Store.js
+++ src/Store.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
 import reducer from './reducer/counter';
 
-// Only add redux-immutable-state-invariant during development;
+// Use redux-immutable-state-invariant to ensure that state mutations are not occurring,
+// as Redux forbids mutations.  Only use it during development though;
 // it involves a lot of object copying and will degrade app performance.
 const middleware = process.env.NODE_ENV === 'production' ?
     [] :
