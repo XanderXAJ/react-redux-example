1b8abd9b5491782e3b607a48ca10624c85e75260
Alex Palmer <xanderx@xanderx.com>
2017-02-15 20:49:55 +0000

    Ensure app state is not mutated during development

--- src/Store.js
+++ src/Store.js
@@ -1,6 +1,15 @@
-import { createStore } from 'redux';
-import counter from './reducer/counter';
+import { applyMiddleware, createStore } from 'redux';
+import reducer from './reducer/counter';
 
-const Store = createStore(counter);
+// Only add redux-immutable-state-invariant during development;
+// it involves a lot of object copying and will degrade app performance.
+const middleware = process.env.NODE_ENV === 'production' ?
+    [] :
+    [require('redux-immutable-state-invariant')()];
+
+const Store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+);
 
 export default Store;
