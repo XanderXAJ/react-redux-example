ec47b396893c74c8d40f974cb46276f6ebc47d9d
Alex Palmer <xanderx@xanderx.com>
2017-02-16 21:48:04 +0000

    Use resolve as this yields an absolute path that webpack desires

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  context: path.join(__dirname, 'src'),
+  context: path.resolve(__dirname, 'src'),
   entry: [
     './main.js'
   ],
   output: {
-    path: path.join(__dirname, 'www'),
+    path: path.resolve(__dirname, 'www'),
     filename: 'bundle.js'
   },
   module: {
