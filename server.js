736ad2e18506e492007c6b58745618a886f8a73b
Alex Palmer <xanderx@xanderx.com>
2017-02-16 22:36:31 +0000

    Note file loading caveat of webpackDevMiddleware

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const express = require('express');
-const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
+const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
 const compiler = webpack(webpackConfig);
 
 app.use(express.static(path.join(__dirname, 'www')));
+
+// Builds and serves files in memory
+// NOTE: If a file exists on disk, the disk version will always be served -- take note after compiling
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
   filename: 'bundle.js',
