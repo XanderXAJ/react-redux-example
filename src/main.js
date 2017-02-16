dc2e33f0a45df2a5905082ea36de6521cc9bf2e5
Alex Palmer <xanderx@xanderx.com>
2017-02-16 23:50:03 +0000

    Remove defunct counter examples

--- src/main.js
+++ src/main.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './Counter';
 import Store from './Store';
 
 const render = () => {
   ReactDOM.render(
-    <Counter
-      value={Store.getState()}
-      onIncrement={() =>
-        Store.dispatch({
-          type: 'INCREMENT'
-        })
-      }
-      onDecrement={() =>
-        Store.dispatch({
-          type: 'DECREMENT'
-        })
-      }
-    />,
-  document.getElementById('root')
+    <div />,
+    document.getElementById('root')
   );
 };
 
