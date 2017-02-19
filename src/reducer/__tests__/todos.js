24a813c554a0889651fa68d07fdd8ebd792f26d6
Alex Palmer <xanderx@xanderx.com>
2017-02-19 21:44:50 +0000

    Add todos tests

---
+++ src/reducer/__tests__/todos.js
@@ -0,0 +1,52 @@
+import todos from '../todos';
+
+test('add todo', () => {
+  const startState = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Todo'
+  };
+  const expectedState = [
+    {
+      completed: false,
+      id: 0,
+      text: 'Todo'
+    }
+  ];
+
+  expect(
+      todos(startState, action)
+  ).toEqual(expectedState);
+});
+
+test('add todo', () => {
+  const startState = [
+    {
+      completed: false,
+      id: 0,
+      text: 'Todo0'
+    }
+  ];
+  const action = {
+    type: 'ADD_TODO',
+    id: 1,
+    text: 'Todo1'
+  };
+  const expectedState = [
+    {
+      completed: false,
+      id: 0,
+      text: 'Todo0'
+    },
+    {
+      completed: false,
+      id: 1,
+      text: 'Todo1'
+    }
+  ];
+
+  expect(
+      todos(startState, action)
+  ).toEqual(expectedState);
+});
