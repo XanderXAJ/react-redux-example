ccdc959b3535eaa7c90a2ebf72e2ef8f3283061a
Alex Palmer <xanderx@xanderx.com>
2017-02-19 14:50:30 +0000

    Move todos reducer to separate file

--- src/reducer/todoApp.js
+++ src/reducer/todoApp.js
@@ -1,20 +1,6 @@
 import { combineReducers } from 'redux';
-import todo from './todo';
+import todos from './todos';
 import visibilityFilter from './visibilityFilter';
-
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ];
-    case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action));
-    default:
-      return state;
-  }
-};
 
 const todoApp = combineReducers({
   todos,
